15754e3760fe3e0ae3d643caa2ad9857e3498039
Máté Lang <mate@MacBook-Pro.localdomain>
2021-04-29 18:46:52 +0300

    First commit

---
+++ main.tf
@@ -0,0 +1,5 @@
+module "dns" {
+  source = "./modules/dns"
+
+  name = "module.hwsw.binhatch.dev"
+}
---
+++ modules/dns/route53.tf
@@ -0,0 +1,4 @@
+resource "aws_route53_zone" "this" {
+  name = var.name
+  delegation_set_id = "N0552769V8EUEN1MEBF3"
+}
